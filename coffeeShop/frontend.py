0463b51243f0ac49dc64b3b3c13be9b0cd56a8f2
Charlie Brewis <charliebrewis7@gmail.com>
2024-02-23 15:37:04 +0000

    Imported folder for Q4

---
+++ coffeeShop/frontend.py
@@ -0,0 +1,111 @@
+from tkinter import *
+from backend import CoffeeShop
+
+
+class CoffeeShopApp:
+    
+    def __init__(self, coffeeShop):
+        self.coffeeShop = coffeeShop
+
+        self.win = Tk()
+        self.win.title("Coffee Shop")
+
+        self.mainFrame = Frame(self.win)
+        self.mainFrame.grid(
+            row=0,
+            column=0,
+            padx=10,
+            pady=10,
+        )
+
+        self.newCustomerName = StringVar()
+
+        self.customerWidgets = []
+
+    def run(self):
+        self.createWidgets()
+        self.win.mainloop()
+
+    def createWidgets(self):
+        self.deleteAllCustomerWidgets()
+
+        customerEntry = Entry(
+            self.mainFrame,
+            textvariable=self.newCustomerName
+        )
+        customerEntry.grid(
+            row=0,
+            column=0,
+            padx=5,
+            pady=5,
+        )
+
+        addCustomerButton = Button(
+            self.mainFrame,
+            text="Add",
+            command=self.addCustomer
+        )
+        addCustomerButton.grid(
+            row=0,
+            column=1,
+            padx=5,
+            pady=5,
+        )
+
+        numCustomers = self.coffeeShop.getNumCustomers()
+        i = 0
+        while i <= numCustomers:
+            customer = self.coffeeShop.getCustomerAt(i)
+            customerLabel = Label(
+                self.mainFrame,
+                text=customer
+            )
+            customerLabel.grid(
+                row=i+1,
+                column=0,
+                padx=5,
+                pady=5,
+            )
+            self.customerWidgets.append(customerLabel)
+
+            removeCustomerButton = Button(
+                self.mainFrame,
+                text="Remove",
+                command=lambda: self.removeCustomer(i)
+            )
+            removeCustomerButton.grid(
+                row=i+1,
+                column=1,
+                padx=5,
+                pady=5,
+            )
+            self.customerWidgets.append(removeCustomerButton)
+            i = i + 1
+
+    def addCustomer(self):
+        name = self.newCustomerName.get()
+        self.coffeeShop.addCustomer(name)
+        self.createWidgets()
+        self.newCustomerName.set("")
+
+    def removeCustomer(self, index):
+        self.coffeeShop.removeCustomerAt(index)
+        self.createWidgets()
+
+    def deleteAllCustomerWidgets(self):
+        for widget in self.customerWidgets:
+            widget.destroy()
+
+
+def main():
+    coffeeShop = CoffeeShop()
+
+    coffeeShop.addCustomer("Alice")
+    coffeeShop.addCustomer("Bob")
+    coffeeShop.addCustomer("Charlie")
+
+    app = CoffeeShopApp(coffeeShop)
+    app.run()
+
+
+main()
